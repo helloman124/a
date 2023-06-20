ccb4d19328e265d01b936d46778e0cba1a3424e6
hellomoto76 <123373882+hellomoto76@users.noreply.github.com>
2023-06-20 21:32:48 +0530

    Update lp03.c

--- lp03.c
+++ lp03.c
@@ -1,38 +1,6 @@
 #include<stdio.h>
 #include<time.h>
 
-int bin_search(int a[],int n,int key);
-
-int main()
-{
-        int n,i,a[100],key,f;
-        double clk;
-        clock_t starttime,endtime;
-
-        printf("\n\t\t\t Binary search \n");
-        printf("\n Enter number of elements");
-        scanf("%d",&n);
-
-        for(i=0;i<n;i++)
-	     	a[i]=rand()%100;
-	 
-	  printf("Enetr the key to be searched:");
-	  scanf("%d",&key); 	
-        
-  	  starttime=clock(); 
- 	  f=bin_search(a,n,key);
-     	  endtime=clock();
-                
-        if(f==-1)
-           printf("\nThe element %d is not found",key);
-        else
-           printf("The element %d is found in position %d",key,f);
-
-        clk=(double)(endtime-starttime)/CLOCKS_PER_SEC;
-        printf("\nThe run time is %f\n",clk);
- }
-  
-
 int bin_search(int a[],int n,int key)
 {
 int low,mid,high;
@@ -49,3 +17,32 @@ while(low<=high)
 }
 return(-1);
 }
+int main()
+{
+        int n,i,a[100],key,f;
+        double clk;
+        clock_t starttime,endtime;
+
+        printf("\n\t\t\t Binary search \n");
+        printf("\n Enter number of elements");
+        scanf("%d",&n);
+
+        for(i=0;i<n;i++)
+	     	a[i]=rand()%100;
+	 
+	  printf("Enter the key to be searched:");
+	  scanf("%d",&key); 	
+        
+  	  starttime=clock(); 
+ 	  f=bin_search(a,n,key);
+     	  endtime=clock();
+                
+        if(f==-1)
+           printf("\nThe element %d is not found",key);
+        else
+           printf("The element %d is found in position %d",key,f);
+
+        clk=(double)(endtime-starttime)/CLOCKS_PER_SEC;
+        printf("\nThe run time is %f\n",clk);
+ }
+  
