da1c034108e750ee5244ab0f1b2e670cf1437e59
hellomoto76 <123373882+hellomoto76@users.noreply.github.com>
2023-07-26 19:20:47 +0530

    Create lp14.c

---
+++ lp14.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int count,w[10],d,x[10];
+// recursive function to find the subsets whose sum of elements is equal to d
+void subset(int cs,int k,int r) // cs = current sum, k = level, r = remaining sum
+{
+   int i;
+   x[k]=1; 
+   if((cs+w[k])==d)
+    {
+       printf("\n Subset solution = %d\n",++count);
+       for(i=0; i<=k; i++)
+       {
+         if(x[i]==1)
+         printf("%d\n",w[i]);  // print the subset
+       }
+     }
+  else if(cs+w[k]+w[k+1] <=d)
+        subset(cs+w[k],k+1,r-w[k]);  
+    if((cs+r-w[k]>=d)&&(cs+w[k+1])<=d)
+    {
+      x[k]=0;
+      subset(cs,k+1,r-w[k]);
+    }
+}
+  
+int main()
+{
+    int sum=0,i,n;
+    printf("enter no of elements\n");
+    scanf("%d",&n);
+    printf("Enter the elements in ascending order\n");
+    for(i=0; i<n; i++)
+    scanf("%d",&w[i]); 
+    printf("Enter the required sum\n");
+    scanf("%d",&d); 
+    for(i=0; i<n; i++)
+    sum +=w[i];  
+ if(sum < d)
+    {
+        printf("no solution exists\n");
+    }
+    printf("The solution is\n");
+    count =0;
+    subset(0,0,sum); // start with level 0, current sum 0, and total sum
+    return 0;
+}
