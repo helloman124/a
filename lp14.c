d8e77b78d0ca782413ef128928197ad4daf51c7a
hellomoto76 <123373882+hellomoto76@users.noreply.github.com>
2023-09-03 23:56:19 +0530

    Update lp14.c

--- lp14.c
+++ lp14.c
@@ -1,47 +1,41 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-int count,w[10],d,x[10];
-// recursive function to find the subsets whose sum of elements is equal to d
-void subset(int cs,int k,int r) // cs = current sum, k = level, r = remaining sum
-{
-   int i;
-   x[k]=1; 
-   if((cs+w[k])==d)
-    {
-       printf("\n Subset solution = %d\n",++count);
-       for(i=0; i<=k; i++)
-       {
-         if(x[i]==1)
-         printf("%d\n",w[i]);  // print the subset
-       }
-     }
-  else if(cs+w[k]+w[k+1] <=d)
-        subset(cs+w[k],k+1,r-w[k]);  
-    if((cs+r-w[k]>=d)&&(cs+w[k+1])<=d)
-    {
-      x[k]=0;
-      subset(cs,k+1,r-w[k]);
-    }
+#define MAX_ELEMENTS 10
+int set[MAX_ELEMENTS];
+int subset[MAX_ELEMENTS];
+int n, targetSum, count = 0;
+void findSubsets(int currentSum, int index) {
+ if (currentSum == targetSum) {
+ count++;
+ for (int i = 0; i < index; i++) {
+ if (subset[i] != 0) {
+ printf("%d ", subset[i]);
+ }
+ }
+ printf("\n");
+ return;
+ }
+ if (index >= n || currentSum > targetSum) {
+ return;
+ }
+ subset[index] = set[index];
+ findSubsets(currentSum + set[index], index + 1);
+ subset[index] = 0;
+ findSubsets(currentSum, index + 1);
 }
-  
-int main()
-{
-    int sum=0,i,n;
-    printf("enter no of elements\n");
-    scanf("%d",&n);
-    printf("Enter the elements in ascending order\n");
-    for(i=0; i<n; i++)
-    scanf("%d",&w[i]); 
-    printf("Enter the required sum\n");
-    scanf("%d",&d); 
-    for(i=0; i<n; i++)
-    sum +=w[i];  
- if(sum < d)
-    {
-        printf("no solution exists\n");
-    }
-    printf("The solution is\n");
-    count =0;
-    subset(0,0,sum); // start with level 0, current sum 0, and total sum
-    return 0;
+int main() {
+ printf("Enter the number of elements in the set: ");
+ scanf("%d", &n);
+ printf("Enter the elements of the set in ascending order:\n");
+ for (int i = 0; i < n; i++) {
+ scanf("%d", &set[i]);
+ }
+ printf("Enter the target sum: ");
+ scanf("%d", &targetSum);
+ printf("The solution subsets are:\n");
+ findSubsets(0, 0);
+ if (count == 0) {
+ printf("No solution exists.\n");
+ }
+ return 0;
 }
